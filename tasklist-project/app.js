769a0dc6ef95b5a7662ec4008558d1869f418bd8
Charlie <charlesclinesdesk@gmail.com>
2020-05-16 09:30:41 -0400

    completed the ui for removing and filtering

--- tasklist-project/app.js
+++ tasklist-project/app.js
@@ -1,6 +1,6 @@
 // DEFINE UI VARS
 const form = document.querySelector('#task-form');
-const tasklist = document.querySelector('.collection');
+const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
@@ -12,11 +12,17 @@ loadEventListeners();
 function loadEventListeners() {
     //Add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 //Add task
 function addTask(e) {
-    if(taskInput.value === '') {
+    if (taskInput.value === '') {
         alert('Add a task');
     }
 
@@ -36,12 +42,47 @@ function addTask(e) {
     li.appendChild(link);
 
     // Append li to ul
-    tasklist.appendChild(li);
+    taskList.appendChild(li);
 
     //Clear input
     taskInput.value = '';
 
-
-
     e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Clear Tasks
+function clearTasks() {
+    // taskList.innerHTML = '';
+
+    // Faster
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+
+
+    // https://jsperf.com/innerhtml-vs-removechild
+
+}
+
+//Filter tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function (task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
